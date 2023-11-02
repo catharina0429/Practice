0e2d3695428144568e75fcfd21897d71cb3c2f75
Jisoo_Park <jisoo.park@nmbu.no>
2023-11-02 15:22:28 +0100

    R on Exercism Leap Year

--- R/R on Exercism.R
+++ R/R on Exercism.R
@@ -14,3 +14,27 @@ two_fer <- function(input) {
 }
 two_fer()
 two_fer("Alice")
+
+## Leap ====
+# on every year that is evenly divisible by 4
+# except every year that is evenly divisible by 100
+# unless the year is also evenly divisible by 400
+
+# leap <- function(year) {
+#   if ((year %% 4 == 0 )|(year %% 400 == 0))
+#     TRUE
+#   else
+#     FALSE
+# }
+
+leap <- function(year) {
+  if ((year %% 4 == 0 && year %% 100 != 0) || year %% 400 == 0) {
+    return(TRUE)
+  } else {
+    return(FALSE)
+  }
+}
+leap(1990)
+leap(2000)
+leap(1996)
+leap(1997)
